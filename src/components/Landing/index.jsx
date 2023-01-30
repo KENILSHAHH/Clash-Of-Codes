ea95e0068f4d2ffb3b4e4279a2f268f6e2f46b10
kenilshahh <kenilshah1505@gmail.com>
2023-01-30 12:47:59 +0530

    button color changed

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -58,7 +58,7 @@ const About = (props) => {
       <h3>{TOP_SECTION.desc}</h3>
      <div crossorigin="anonymous"
                 className="apply-button"
-                data-hackathon-slug="https://clash-of-codes.devfolio.co/"
+                data-hackathon-slug="clash-of-codes.devfolio.co/"
                 data-button-theme="light"
                 style={{ height: "44px", width: "312px" }}
 ></div>
