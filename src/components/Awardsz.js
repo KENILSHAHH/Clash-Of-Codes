ca4d4827e70ec9ed5fe523bdaccb739f3fc6c6f9
kenilshahh <kenilshah1505@gmail.com>
2023-02-07 21:41:56 +0530

    footjs updated

--- src/components/Awardsz.js
+++ src/components/Awardsz.js
@@ -19,7 +19,7 @@ function PrizeHeading() {
               alignItems: 'center'
             }}
           >
-            <Glitch color='lightBlue'>Prizes</Glitch>{' '}
+            <Glitch color='lightGreen'>Prizes</Glitch>{' '}
           </h1>
         </div>
         <div className="row">
@@ -139,7 +139,7 @@ function PrizeHeading() {
                 alignItems: 'center'
               }}
             >
-              <Glitch color='lightGreen'>Domain Prizes</Glitch>{' '}
+              <Glitch color='red'>Domain Prizes</Glitch>{' '}
             </h1>
           </div>
           <div className="col-sm-6 col-md-4 p-5">
